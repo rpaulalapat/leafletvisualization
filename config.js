6c559e6fa5c3fdd6d1c9698f6570344bb32c99aa
rpaulalapat <rpaulalapat@gmail.com>
2018-07-23 18:04:51 -0500

    part 1 solution

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicnBhdWxhbGFwYXQiLCJhIjoiY2pqMWppeWN4MHExZjNxbzRjcnNyZjBpaiJ9.xrBekB1zcYxfMXzQXrr5Jw";
---
+++ logic.js
@@ -0,0 +1,73 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [39.7128, -76.0059],
+  zoom: 4
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Link to GeoJSON
+var APILink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+var geojson;
+
+// Grabbing data with d3...
+d3.json(APILink, function(data) {
+
+  // Creating a new choropleth layer
+  geojson = L.choropleth(data, {
+    // Which property in the features to use
+    valueProperty: "mag",
+    // Color scale
+    scale: ["#ffffb2", "#b10026"],
+    // Number of breaks in step range
+    steps: 10,
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Mag: "+feature.properties.mag + "<br>" + feature.properties.place);
+    }
+  }).addTo(myMap);
+
+  // Setting up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Earthquake Magnitude</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(myMap);
+
+});
